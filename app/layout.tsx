4adab9dbc602699c9f1ec6840714eaa12b204569
siraom15 <siriwatjaiyungyuen@gmail.com>
2025-02-10 02:19:14 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_Thai_Looped } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const notoSansThaiLooped = Noto_Sans_Thai_Looped({
+  weight: "400",
+  subsets: ["thai"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={notoSansThaiLooped.className}>{children}</body>
     </html>
   );
 }
